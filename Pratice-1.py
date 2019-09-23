43d2d8a82a73cb3a403b4e7f249e103c98f4840a
HanumanP01 <hanuman.pvdg@gmail.com>
2019-09-23 19:17:22 +0530

    Python files add

---
+++ Pratice-1.py
@@ -0,0 +1,25 @@
+# This my pratice-1
+
+a = 5
+b = 3.5
+c = 'H'
+
+print ( type(a) )
+print ( type(b) )
+print ( type(c) )
+
+d = a + b
+print ( type(d))
+
+b = int (b)
+
+print ( type(b))
+
+a = str(a)
+d = a + c
+print (d)
+
+print ( 5 == 5 , 2 == 3 )
+
+
+
---
+++ Pratice-2.py
@@ -0,0 +1,88 @@
+# Pratice-1
+
+# Number swap
+
+""" a = int (input ("Enter A value:"))
+b = int (input ("Enter B value:"))
+
+temp = a
+a = b
+b = temp
+
+option  = str(input("select a or b "))
+
+if option == 'a':
+    print(a)
+elif option == 'b':
+    print(b)
+else:
+    print("Invalid option") """
+
+
+# Fibonacci 
+
+""" a = int (input ("Enter 1st value:"))
+b = int (input ("ENter 2nd Value:"))
+a1 = a
+a2 = a + 1
+print (a1,a2, sep='\n',end='-\n')
+for i in range (a,b):
+    i = a1 + a2
+    a1 = a2
+    a2 = i 
+    print (i)   """
+
+
+# Prime numbers 1 to 100
+# a = int (input ("Enter value:"))
+# b = int (input ("ENter 2nd Value:"))
+
+""" if a % 2 == 0 :
+    print ("Given number is not a Prime")
+elif a == 3 :
+    print ("Given number Prime number")
+elif a % 3 == 0 :
+    print ("Given number is not a Prime")
+elif a == 5 :
+    print ("Given number Prime number")
+elif a % 5 == 0 :
+    print ("Given number is not a Prime")
+elif a % 6 == 0 :
+    print ("Given number is not a Prime")
+elif a == 7 :
+    print ("Given number Prime number")
+elif a % 7 == 0 :
+    print ("Given number is not a Prime")
+elif a % 8 == 0 :
+    print ("Given number is not a Prime")
+elif a % 9 == 0 :
+    print ("Given number is not a Prime")
+elif a % 10 == 0 :
+    print ("Given number is not a Prime")
+else :
+    print ("Given number is prime") """
+
+h = int (input ("Enter first number"))
+k = int (input ("Enter second number"))
+for a in range (h,k)
+b = a // 2
+c = b + 1
+f = 0
+for i in range (2,c):
+    if a % i == 0:
+        f=f+1
+        break
+print (
+if(f == 0):
+    print ("Given number is Prime")
+else :
+    print (" Given number is not a prime")
+    
+
+
+
+
+
+
+
+
